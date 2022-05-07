c30402ee4a94be6a718adc1784778b7cb676a64c
Lff_DesktopPC <scut_lff@163.com>
2022-05-07 13:03:31 +0800

    first commit

---
+++ src/main/java/com/atlff/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atlff.git;
+
+/**
+ * @author lifeifan
+ * @Description
+ * @create 2022-05-07 12:58
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
