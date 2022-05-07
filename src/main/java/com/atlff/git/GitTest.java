0003da9a3c1bfe5d068c23ebeed990c67d4b25f1
Lff_DesktopPC <scut_lff@163.com>
2022-05-07 13:08:54 +0800

    third commit

--- src/main/java/com/atlff/git/GitTest.java
+++ src/main/java/com/atlff/git/GitTest.java
@@ -2,12 +2,13 @@ package com.atlff.git;
 
 /**
  * @author lifeifan
- * @Description
+ * @Description git版本控制测试
  * @create 2022-05-07 12:58
  */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git!");
         System.out.println("hello,git2!");
+        System.out.println("hello,git3!");
     }
 }
